43e586b3bdbc6ec374e11bf2ab1ef7cd7189e7f6
Simonek, Jan <jsimonek@netsuite.com>
2021-08-31 16:26:26 +0200

    CracklePopTest - replaced getNthElement(n) by a direct call to cracklePop.getElement(n)

--- src/test/java/CracklePopTest.java
+++ src/test/java/CracklePopTest.java
@@ -15,41 +15,32 @@ class CracklePopTest
 	@Test
 	void sequenceStartsWith1()
 	{
-		Assertions.assertEquals(getNthElement(1), "1");
+		Assertions.assertEquals(cracklePop.get(), "1");
 	}
 
 	@Test
 	void secondElementIs2()
 	{
-		Assertions.assertEquals(getNthElement(2), "2");
+		cracklePop.get();
+		Assertions.assertEquals(cracklePop.get(), "2");
 	}
 
 	@Test
 	void thirdElementIsCrackle()
 	{
-		Assertions.assertEquals(getNthElement(3), "Crackle");
+		Assertions.assertEquals(cracklePop.getElement(3), "Crackle");
 	}
 
 	@Test
 	void elementsDivisibleByThreeAreCrackle()
 	{
-		Assertions.assertEquals(getNthElement(6), "Crackle");
-		Assertions.assertEquals(getNthElement(912), "Crackle");
+		Assertions.assertEquals(cracklePop.getElement(6), "Crackle");
+		Assertions.assertEquals(cracklePop.getElement(912), "Crackle");
 	}
 
 	@Test
 	void fifthElementIsPop()
 	{
-		Assertions.assertEquals(getNthElement(5), "Pop");
-	}
-
-	private String getNthElement(int n)
-	{
-		for (int i = 0; i < n -1; ++i)
-		{
-			cracklePop.get();
-		}
-
-		return cracklePop.get();
+		Assertions.assertEquals(cracklePop.getElement(5), "Pop");
 	}
 }
